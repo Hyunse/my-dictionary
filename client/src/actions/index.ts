a188ca80998a646659fec3632531dcbbfc568f70
KimHyunse <anzmf12@naver.com>
2018-11-11 01:24:07 -0500

    Add comments & remove consloe.log

--- client/src/actions/index.ts
+++ client/src/actions/index.ts
@@ -1,16 +1,19 @@
 import axios from 'axios';
 import { API_ERROR, API_SUCCESS } from './types';
 
-export const searchWords = (searchValue) => async (dispatch) => {
+/**
+ * Search Word
+ * @param {string} searchValue
+ * @desc call merriam webster dictionary api
+ */
+export const searchWords = (searchValue: string) => async (dispatch) => {
   try {
     const response = await axios.get(
       `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${searchValue}?key=${
         process.env.REACT_APP_DICTIONARY_KEY
       }`
     );
-    // tslint:disable-next-line
-    console.log(response.data);
-
+    
     dispatch({
       payload: response.data,
       type: API_SUCCESS
