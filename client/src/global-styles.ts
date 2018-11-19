c7b383041e2afb819a773493f45a5005e64562e9
KimHyunse <anzmf12@naver.com>
2018-11-19 12:57:26 -0500

    Add GoogleFont ( OpenSans, Playfair )

--- client/src/global-styles.ts
+++ client/src/global-styles.ts
@@ -4,6 +4,7 @@ import { createGlobalStyle } from './typed-components';
 // tslint:disable-next-line
 const GlobalStyle = createGlobalStyle`
   ${reset}
+  @import url('https://fonts.googleapis.com/css?family=Open+Sans|Playfair+Display:400,700');
   html,body {
     padding: 0;
     margin: 0;
