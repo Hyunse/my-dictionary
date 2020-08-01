7f700f401c63f326b313da8f727c62c03a332286
Kim Hyunse / Arthur <anzmf12@naver.com>
2020-08-01 16:54:57 -0400

    Change user route controller

--- server/src/controllers/controller_user.ts
+++ server/src/controllers/controller_user.ts
@@ -1,5 +1,3 @@
-import { Request, Response } from 'express';
-import asyncHandler from '../middlewares/asyncHandler';
 import PasswordUtil from '../utils/util_password';
 import Models from '../models';
 import { User } from '../models/Users';
@@ -8,22 +6,14 @@ import JwtUtil from '../utils/util_jwt';
 class UserController {
   constructor() {}
 
-  public findAllUsers = asyncHandler(async (_: Request, res: Response) => {
-    const users = await Models.user.findAll();
-    res.send({
-      ok: true,
-      data: users
-    });
-  });
+  public findAllUsers = async () => {
+    return await Models.user.findAll();
+  };
 
   /**
    * Sign In
    */
-  public signIn = asyncHandler(async (req: Request, res: Response) => {
-    // Param
-    const email = req.body.email;
-    const password = req.body.password;
-
+  public signIn = async (email: string, password: string) => {
     // Find User By Email
     const user: User = await Models.user
       .findOne({ where: { email: email } })
@@ -35,42 +25,35 @@ class UserController {
       throw {
         status: 401,
         message: 'Email Not Found',
-        token: null
+        token: null,
       };
     }
 
     const isSame = await PasswordUtil.comparePassword(password, user.password);
 
-    if (isSame) {
-      // Create JWT
-      const token = JwtUtil.createJWT(user.id);
-
-      res.send({
-        ok: true,
-        message: null,
-        token
-      });
-    } else {
+    if (!isSame) {
       throw {
         ok: false,
         status: 401,
         message: `Password is incorrect.`,
-        token: null
+        token: null,
       };
     }
-  });
+
+    // Create JWT
+    const token = JwtUtil.createJWT(user.id);
+
+    return token;
+  };
 
   /**
    * Sign Up
    */
-  public signUp = asyncHandler(async (req: Request, res: Response) => {
-    // Param
-    let user = req.body;
-
+  public signUp = async (user: any) => {
     const duplicatedUser = await Models.user.findOne({
       where: {
-        email: user.email
-      }
+        email: user.email,
+      },
     });
 
     if (duplicatedUser) {
@@ -78,7 +61,7 @@ class UserController {
         ok: false,
         status: 409,
         message: `Email already exist.`,
-        token: null
+        token: null,
       };
     }
 
@@ -90,38 +73,33 @@ class UserController {
       return res;
     });
 
+    // Create JWT
     const token = JwtUtil.createJWT(newUser.id);
 
-    res.send({
-      ok: true,
-      message: null,
-      token
-    });
-  });
+    return token;
+  };
 
   /**
    * Update User Info
    */
-  public update = asyncHandler(async (req: Request, res: Response) => {
-    // Param
-    const name = req.body.name;
-    const email = req.body.email;
-    const country = req.body.country;
-    const password = req.body.password;
-    // Jwt
-    const user = req.body.user;
-
+  public update = async (
+    name: string,
+    email: string,
+    country: string,
+    password: string,
+    id: number
+  ) => {
     const result = await Models.user.update(
       { name, email, country, password },
       {
         where: {
-          id: user.id
-        }
+          id,
+        },
       }
     );
 
-    res.send(result);
-  });
+    return result;
+  };
 }
 
 export default new UserController();
--- server/src/routes/route_user.ts
+++ server/src/routes/route_user.ts
@@ -1,15 +1,69 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 import asyncHandler from '../middlewares/asyncHandler';
 import userController from '../controllers/controller_user';
 
 const router: Router = Router();
 
-router.get('/users', asyncHandler(userController.findAllUsers));
+router.get(
+  '/users',
+  asyncHandler(async (_: Request, res: Response) => {
+    const result = await userController.findAllUsers();
 
-router.post('/user/signIn', asyncHandler(userController.signIn));
+    res.send({
+      ok: true,
+      data: result,
+    });
+  })
+);
 
-router.post('/user/signUp', asyncHandler(userController.signUp));
+router.post(
+  '/user/signIn',
+  asyncHandler(async (req: Request, res: Response) => {
+    // Param
+    const email = req.body.email;
+    const password = req.body.password;
 
-router.post('/user/update', asyncHandler(userController.update));
+    const result = await userController.signIn(email, password);
+
+    res.send({
+      ok: true,
+      message: null,
+      token: result,
+    });
+  })
+);
+
+router.post(
+  '/user/signUp',
+  asyncHandler(async (req: Request, res: Response) => {
+    // Param
+    let user = req.body;
+
+    const result = await userController.signUp(user);
+
+    res.send({
+      ok: true,
+      message: null,
+      token: result,
+    });
+  })
+);
+
+router.post(
+  '/user/update',
+  asyncHandler(async (req: Request, res: Response) => {
+    // Param
+    const name = req.body.name;
+    const email = req.body.email;
+    const country = req.body.country;
+    const password = req.body.password;
+    // Jwt
+    const { id } = req.body.user;
+
+    const result = userController.update(name, email, country, password, id);
+
+    res.send(result);
+  })
+);
 
 export default router;
